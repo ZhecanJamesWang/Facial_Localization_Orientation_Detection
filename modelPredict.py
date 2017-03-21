b48e8a72f76b5a94288f7b9d41a12c2f2839c621
zhecan wang <zhecanwang@199-218.priv23.nus.edu.sg>
2017-03-21 13:38:25 +0800

    update

--- modelPredict.py
+++ modelPredict.py
@@ -129,7 +129,7 @@ class ModelPredict(object):
                 labelImg = ut.plotTarget(img, ut.deNormalize(labels, self.imSize), self.imSize, ifSquareOnly = True)
                 cv2.imwrite(self.evaluationOutputDir + 'inputTestImg' + str(iter * self.batch_size + i) + '.jpg', img)
                 labelImg = ut.plotTarget(img, ut.deNormalize(pred[i], self.imSize), self.imSize, ifSquareOnly = True)
-                print 'save predTestLabelImg' + str(testCount) + '.jpg to: ' + self.outputDir
+                print 'save predTestLabelImg' + str(iter * self.batch_size + i) + '.jpg to: ' + self.outputDir
                 cv2.imwrite(self.evaluationOutputDir + 'predTestLabelImg' + str(iter * self.batch_size + i) + '.jpg', labelImg)
 
 
