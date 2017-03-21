6c8f5f7ad42d7be091744cc5d9a28a583feccb63
zhecan wang <zhecanwang@118-238.priv23.nus.edu.sg>
2017-03-21 20:38:51 +0800

    update

--- preProcess.py
+++ preProcess.py
@@ -13,23 +13,23 @@ class PreProcess(object):
 	def __init__(self):
 		self.rawDataDir = "/home/james/Menpo39_Valid"
 
-	 def getDataByFiles(self):
-	        counter = 0
-	        files = os.listdir(self.rawDataDir)
+	def getDataByFiles(self):
+	    counter = 0
+	    files = os.listdir(self.rawDataDir)
 
-	        for file in files:
-	            if file != ".DS_Store" and self.format in file:
-	            	if ".pts39" in file:
-	            		print "file name: ", file
-	            	# P39=np.loadtxt(pts39)
-	             #    # imgs, landmarks = self.extract(path + "/", file)
-	             #    imgs, landmarks = self.extract(self.rawDir + "/", file)   
-	             #    if not self.debug:                 
-	             #        self.saveImg(imgs, landmarks, file)
-	             #    counter += 1
+	    for file in files:
+	        if file != ".DS_Store" and self.format in file:
+	        	if ".pts39" in file:
+	        		print "file name: ", file
+	        	# P39=np.loadtxt(pts39)
+	         #    # imgs, landmarks = self.extract(path + "/", file)
+	         #    imgs, landmarks = self.extract(self.rawDir + "/", file)   
+	         #    if not self.debug:                 
+	         #        self.saveImg(imgs, landmarks, file)
+	         #    counter += 1
 
-	             #    if counter % 100 == 0:
-	             #        print counter
-	             #        # print path
+	         #    if counter % 100 == 0:
+	         #        print counter
+	         #        # print path
 
 
