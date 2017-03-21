75d2551fb0fa0977f808e8920d068adcadbd62d4
zhecan wang <zhecanwang@199-218.priv23.nus.edu.sg>
2017-03-21 12:35:17 +0800

    update

--- modelPredict.py
+++ modelPredict.py
@@ -14,7 +14,6 @@ class ModelPredict(object):
         self.model = m.model(input_shape=(self.imSize, self.imSize, 3), weights_path = self.weightPath)
         sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9)
         self.model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy', self.final_pred])
-        loaded_model.evaluate(X, Y, verbose=0)
 
 
     def final_pred(self, y_true, y_pred):
