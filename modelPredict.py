bebed2a24f7facdb6e813b33b0af36a84b3fbc82
zhecan wang <zhecanwang@112-174.priv23.nus.edu.sg>
2017-03-22 22:57:57 +0800

    update

--- modelPredict.py
+++ modelPredict.py
@@ -14,7 +14,7 @@ class ModelPredict(object):
         self.imSize = 256
         self.evaluationOutputDir = "./03222017_02_Menpo39_evaluation_output/"
 
-        self.weightPath = "./03202017_01_square_model/model39000.h5"
+        self.weightPath = "./03202017_02_square_add_layers_model/model39000.h5"
         self.model = m.model(input_shape=(self.imSize, self.imSize, 3), weights_path = self.weightPath)
         sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9)
         self.model.compile(loss='mean_squared_error', optimizer=sgd, metrics=['accuracy', self.final_pred])
