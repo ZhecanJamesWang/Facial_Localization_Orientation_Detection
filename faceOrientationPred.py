e682e66641916a3c22b61d3e037f77b1aae1f457
zhecan wang <zhecanwang@199-29.priv23.nus.edu.sg>
2017-03-22 11:15:23 +0800

    update

--- faceOrientationPred.py
+++ faceOrientationPred.py
@@ -84,7 +84,7 @@ class faceOrientPred(object):
     def readMenpo39DataSet(self):
         self.ImgDir = "./Menpo39Preprocessed/img/"
         self.PTSDir = "./Menpo39Preprocessed/pts/"
-        self.imgs = os.listdir(PTSDir)
+        self.imgs = os.listdir(self.PTSDir)
         # self.pts = os.listdir(ImgDir)
         # assert len(pts) == len(imgs)
 
