44db5d01c1dfa621b70c5ecc0c659ddff1536529
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2017-03-30 19:35:56 +0800

    update

---
+++ combineTxt.py
@@ -0,0 +1,7 @@
+
+f1 = open("/home/james/Facial_Localization_Orientation_Detection/data/Menpo39TrainProcessed/menpo39Data.txt", 'r')
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'a')
+lines = f1.readlines()
+for line in lines:
+	f2.write()
+f.close()
