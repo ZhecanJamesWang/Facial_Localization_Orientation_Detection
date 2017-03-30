7ca7dcd71763223497646c82ae34f5859b74022f
zhecan wang <zhecanwang@114-235.priv23.nus.edu.sg>
2017-03-30 20:11:19 +0800

    update

--- combineTxt.py
+++ combineTxt.py
@@ -1,18 +1,17 @@
 
 
-f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'r')
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/data/KBKC4_train.txt", 'r')
 lines = f2.readlines()
 print "f2: ", len(lines)
 
 f1 = open("/home/james/Facial_Localization_Orientation_Detection/data/Menpo39TrainProcessed/menpo39Data.txt", 'r')
-f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'a')
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/data/KBKC4_train.txt", 'a')
 lines = f1.readlines()
 for line in lines:
-	# print line
 	f2.write(line)
 f2.close()
 f1.close()
 
-f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'r')
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/data/KBKC4_train.txt", 'r')
 lines = f2.readlines()
 print "f2: ", len(lines)
