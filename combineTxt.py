12721fb4f6979f8c50c918cd2e06f96eaca442be
zhecan wang <zhecanwang@zhecans-MacBook-Pro.local>
2017-03-30 19:41:19 +0800

    update

--- combineTxt.py
+++ combineTxt.py
@@ -1,4 +1,9 @@
 
+
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'a')
+lines = f2.readlines()
+print "f2: ", len(lines)
+
 f1 = open("/home/james/Facial_Localization_Orientation_Detection/data/Menpo39TrainProcessed/menpo39Data.txt", 'r')
 f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'a')
 lines = f1.readlines()
@@ -7,3 +12,7 @@ for line in lines:
 	f2.write(line)
 f2.close()
 f1.close()
+
+f2 = open("/home/james/Facial_Localization_Orientation_Detection/dataKBKC4_train.txt", 'a')
+lines = f2.readlines()
+print "f2: ", len(lines)
